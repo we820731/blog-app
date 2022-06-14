797ac47d712b9baddeb051d201a9def4298a8ec1
Yining <we820731@gmail.com>
2022-06-14 18:03:26 +0800

    新增 user

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'is_admin',
         'password',
     ];
 
@@ -39,6 +40,7 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'email_verified_at' => 'immutable_datetime',
+        'is_admin'          => 'boolean',
     ];
 }
